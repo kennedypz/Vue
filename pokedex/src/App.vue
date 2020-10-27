1fa98d25f1e62a326d737cf4983e5be1507addfc
Paulo <paulo_lavigne@hotmail.com>
2020-10-27 20:37:13 -0300

    Implementação da busca

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
 
-    <div v-for="(poke, index) in pokemons" :key="index">
-      <Pokemon :pokedexNumber="index+1" :name="poke.name" :url="poke.url"/>
+    <img src="./assets/images/Pokedex.png">
+
+    <div class="column is-half is-offset-one-quarter">
+
+      <div class="field has-addons">
+
+        <div class="control has-icons-left has-icons-right is-expanded">
+            <input type="text" class="input is-rounded"  v-model="busca" placeholder="Buscar Pokémon pelo nome">
+        </div>
+
+        <p class="control">
+            <a class="button is-info is-rounded" @click="buscar">BUSCAR</a>
+        </p>
+
+      </div>
+      
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :pokedexNumber="index+1" :name="poke.name" :url="poke.url"/>
+      </div>
+
     </div>
 
   </div>
@@ -21,26 +39,50 @@ export default {
   data(){
     return {
       pokemons: [],
+      filteredPokemons: [],
+      busca: '',
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(
       response => {
-        console.log("Pegou a lista de pokémons");
         this.pokemons = response.data.results;
+        this.filteredPokemons = response.data.results;
       }
     );
-  }
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      } else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.match(this.busca.toLocaleLowerCase()))
+      }
+    }
+  },
+  // computed: {
+  //  busca em "tempo real"
+  //   resultadoBusca: function(){
+  //     if(this.busca == '' || this.busca == ' '){
+  //       return this.pokemons;
+  //     } else{
+  //       return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+  //     }
+  //   }
+  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
 </style>
--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>
-      <h1>{{pokedexNumber}} {{ name | upper }}</h1>
-      <small>{{ url }}</small>
+  <div id="cardPokemon">
+        <div class="card">
+        <div class="card-image">
+            <figure>
+            <img :src="currentImg" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            <div class="media-content">
+                <p class="title is-4">{{pokedexNumber}} - {{ name | upper }}</p>
+                <p class="subtitle is-6">{{ pokemon.type }}</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <button class="button is-primary is-rounded" @click="mudarImagem">Girar imagem</button>
+        </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+import axios from 'axios';
+
 export default {
+    created: function(){
+        axios.get(this.url).then(
+            response => {
+                this.pokemon.type = response.data.types[0].type.name;
+                this.pokemon.front = response.data.sprites.front_default;
+                this.pokemon.back = response.data.sprites.back_default;
+                this.currentImg = this.pokemon.front;
+            }
+        );
+    },
+    data(){
+        return {
+            isFront: true,
+            currentImg: "",
+            pokemon: {
+                type: "",
+                front: "",
+                back: ""
+            }
+        }
+    },
     props:{
         pokedexNumber: Number,
         name: String,
@@ -17,10 +57,23 @@ export default {
         upper: function(value){
             return value[0].toUpperCase() + value.slice(1);
         }
+    },
+    methods:{
+        mudarImagem: function(){
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back;
+            } else{
+                this.isFront = true;
+                this.currentImg = this.pokemon.front;
+            }
+        }
     }
 }
 </script>
 
 <style>
-
+    #cardPokemon{
+        margin-top: 2%;
+    }
 </style>
